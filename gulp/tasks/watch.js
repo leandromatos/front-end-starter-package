b507431b5057f1607f4b3d6f93c8cf171a4f76e1
Leandro Matos <leandro@is-uz.com>
2017-02-05 10:33:10 -0200

    Fix for watching Pug files

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,7 @@ let Watch = (config, args, log, error, success) => {
     gulp.task('watch', () => {
         gulp.watch('./src/scripts/**/*.{js,vue}', ['scripts'])
         gulp.watch('./src/styles/**/*.scss', ['styles'])
-        gulp.watch('./src/views/**/*.{jade,json}', ['views'])
+        gulp.watch('./src/views/**/*.{pug,json}', ['views'])
     })
 
 }
